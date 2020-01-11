235bbc8230ffca124f2eb53d764ea1c6eb9a000c
bryantutiven2 <bryan.tutiven.2@outlook.com>
2020-01-11 16:21:59 -0500

    Desarrollo de paginacion y busqueda de productos en sección Productos

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # proyectoDAW
+
+Este proyecto está separado en dos carpetas importantes una es el frontend (del lado del cliente) y
+la otra es el backend (del lado del servdior) es importante decir que para el backend existiran otras carpetas
+como Django para manejar la seccion de administrador y crear el modelo de nuestra base de datos, Express para manejar los peticiones y respuesta.
--- frontend/src/components/paginaProductos/PaginacionProductos.vue
+++ frontend/src/components/paginaProductos/PaginacionProductos.vue
@@ -3,9 +3,9 @@
     <div class="row d-flex justify-content-center">
       <!-- Entrada de texto con boton -->
       <div class="input-group col-md-5">
-        <input type="text" class="form-control" placeholder="Ingrese producto..."  aria-label="Ingrese producto..." aria-describedby="button-addon2">
+        <input type="text" id="entradaBuscar" @keyup.enter="resultadoBusqueda" class="form-control" placeholder="Ingrese producto..."  aria-label="Ingrese producto..." aria-describedby="button-addon2">
         <div class="input-group-append">
-          <button class="btn btn-success" type="button" id="button-addon2" >Buscar</button>
+          <button class="btn btn-success" type="button" id="buscar" @click="resultadoBusqueda">Buscar</button>
         </div>
       </div>
     </div>
@@ -14,7 +14,7 @@
     <br>
     <!-- Listar Productos -->
     <div id="container"  class="row  text-center">
-        <div class='col-md-3' id="product.id" v-for="product in listaProductos" :key="product">
+        <div class='col-md-3' id="product.id" v-for="product in displayProductos" :key="product.id">
            <div class="card mb-4 shadow-sm border-top-0 border-right-0 border-bottom-0 border-info">
             <img class="card-img-top promo" id="adv"  :src="product.url"   alt="">
             <div class="card-body">
@@ -31,8 +31,31 @@
 
     <!--Paginacion-->
     <div class="row d-flex justify-content-center">
-      <div class="mt-3">
-        <b-pagination-nav number-of-pages="10" base-url="#"></b-pagination-nav>
+      <div class="btn-group">
+          <b-button
+              type="button"
+              :disabled="page==1"
+              @click="page--"
+              variant="light"
+              class="bg-white text-primary">
+              {{izquierda1}}
+          </b-button>
+          <b-button
+              type="button"
+              v-for="pageNumber in pages.slice(page-1, limitePage)" :key="pageNumber"
+              @click="page=pageNumber"
+              variant="light"
+              class=" bg-white text-dark">
+              {{pageNumber}}
+          </b-button>
+          <b-button
+              type="button"
+              @click="page++"
+              :disabled="page==pages.length"
+              variant="light"
+              class="bg-white text-primary">
+              {{derecha1}}
+          </b-button>
       </div>
     </div>
   </div>
@@ -40,30 +63,89 @@
 </template>
 
 <script>
+  const $ = require('jquery')
+  window.$ = $
+
   import productosJ from '../../../public/json/productos.json';
   export default {
     data(){
       return{
+          izquierda1: '<',
+          derecha1: '>',
+          izquierda: '<<',
+          derecha: '>>',
+          productos: [], //articles
           listaProductos: productosJ,
           numeroProductos: 0,
+          page: 1,
           limitePage: 8,
-          totalPaginas: 0
+          totalPaginas: 0, //perPage
+          pages: []
       }
     },
     methods:{
+      obtenerProductos(){
+        this.productos =productosJ;
+        return this.productos;
+      },
       contarProductos(){
         this.numeroProductos = this.listaProductos.length;
         console.log('Numero de productos es: '+this.numeroProductos);
         return this.numeroProductos;
       },
       sacarNumeroPaginas(){
-          this.totalPaginas = Math.round(this.numeroProductos/this.limitePage);
+          this.totalPaginas = Math.ceil(this.numeroProductos/this.limitePage);
+          console.log(this.totalPaginas)
           return this.totalPaginas;
+      },
+      paginate(productos){
+          let page = this.page;
+          let perPage = this.limitePage;
+          let from = (page * perPage) - perPage;
+          let to = (page * perPage);
+          return productos.slice(from, to);
+
+      },
+      setProductos(){
+          let numberOfPages = this.totalPaginas;
+          for(let i=1; i<=numberOfPages; i++){
+              this.pages.push(i);
+          }
+      },
+      resultadoBusqueda(){
+          let search = document.getElementById("entradaBuscar").value;
+          let productoSearch = search.toLowerCase();
+          console.log(productoSearch);
+          if(productoSearch.length>0){
+              var divs = $('.col-md-3');
+              divs.each(function(){
+                var h5 = $(this).find('h5') 
+                if(h5.text().toLowerCase().indexOf(productoSearch)>=0) {
+                  $(this).show()
+                }
+                else {
+                  $(this).hide()
+                }
+
+              })
+          }
+          return false;
       }
     },
     mounted(){
+      this.obtenerProductos(),
       this.contarProductos(),
       this.sacarNumeroPaginas()
+    },
+    computed:{
+        displayProductos(){
+          return this.paginate(this.productos)
+        }
+    },
+    watch:{
+      productos(){
+        this.setProductos();
+      }
     } 
   }
 </script>
